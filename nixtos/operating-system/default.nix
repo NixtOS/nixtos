b167f34f54ac366e021d7348cd4f3f0b0055629c
Léo Gaspard <leo@gaspard.io>
2018-01-19 12:12:04 +0100

    [operating-system] Add attempt to use an initrd

--- nixtos/operating-system/default.nix
+++ nixtos/operating-system/default.nix
@@ -4,10 +4,31 @@
 }:
 
 let
+  mkInitrd = import ../make-initrd { inherit pkgs; };
+
   # TODO: add nixtos version here
   version = "nixtos-${pkgs.lib.nixpkgsVersion}";
+
+  initScript = pkgs.writeText "init" ''
+    #!${pkgs.busybox} sh
+
+    echo "It works!"
+
+    while true; do
+      sleep 1
+    done
+  '';
+
+  initrd = pkgs.makeInitrd {
+    contents = [
+      { object = initScript;
+        symlink = "/init";
+      }
+    ];
+  };
 in
 pkgs.runCommand version {} ''
   mkdir $out
   ln -s ${kernel} $out/kernel
+  ln -s ${initrd} $out/initrd
 ''
