13c0b136ee11f66ea37040a6adf45779ad7248fe
Léo Gaspard <leo@gaspard.io>
2018-01-21 02:54:48 +0100

    [make-initrd] Minimize weight of the initramfs

--- nixtos/make-initrd/default.nix
+++ nixtos/make-initrd/default.nix
@@ -7,15 +7,59 @@ let
     rootModules = modules;
   };
 
-  # TODO: build busybox as static and make sure that glibc no longer is in the
-  # closure (just building busybox as static isn't enough)
+  # TODO: add a check so that different libraries imported with the same name
+  # couldn't conflict
+  utils = pkgs.runCommandCC "initrd-utils" {
+    buildInputs = [ pkgs.nukeReferences pkgs.glibc ];
+    allowedReferences = [ "out" ];
+  } ''
+    echo "Setting up the environment"
+    mkdir -p $out/{bin,lib}
+    function copy_bin() {
+      cp -fpd "$@" $out/bin
+    }
+    function copy_lib() {
+      cp -fpdL "$@" $out/lib
+    }
+    export -f copy_lib
+
+    echo "Copying BusyBox"
+    for f in ${pkgs.busybox}/{s,}bin/*; do
+      copy_bin "$f"
+    done
+
+    echo "Copying relevant libraries"
+    copy_lib ${pkgs.glibc.out}/lib/ld*.so.?
+    find $out/{bin,lib} -type f | while read f; do
+      ldd "$f" | \
+        awk '{ if ($3 != "") print $3 }' | \
+        xargs -I {} bash -c "copy_lib {}" \
+        || true # Error here basically means static executable
+    done
+
+    echo "Stripping down everything"
+    chmod -R u+w $out
+    stripDirs "lib bin" "-s"
+
+    echo "Nuke references to everything but us"
+    find $out/{bin,lib} -type f | xargs nuke-refs -e $out
+
+    echo "Reset interpreter"
+    find $out/{bin,lib} -type f | \
+      xargs patchelf --set-interpreter $out/lib/ld*.so.? --set-rpath $out/lib \
+      || true # Error here basically means static executable
+
+    echo "Testing patched programs"
+    $out/bin/ash -c 'echo "Test"' | grep 'Test' > /dev/null
+  '';
+
   # TODO: get rid of these ugly “virtio_blk: Unknown symbol
   # register_virtio_driver (err 0)” & co. errors (by switching to non-busybox
   # modprobe? see [1])
   # [1] https://github.com/quitesimpleorg/N900_RescueOS/commit/0c3ce0d7b46a32e460a4ba6dd8f2799cd68c5c33
   init = pkgs.writeScript "initrd-init" ''
-    #!${pkgs.busybox}/bin/sh
-    PATH="${pkgs.busybox}/bin"
+    #!${utils}/bin/ash
+    PATH="${utils}/bin"
 
     echo "Setting up basic environment"
     mount -t devtmpfs none /dev
