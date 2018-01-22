0dcbe3eb2acc1eff7c3cdc4d9ce99369504f0540
Léo Gaspard <leo@gaspard.io>
2018-01-22 04:28:57 +0100

    [runit] Generate test /etc/runit/1 script

--- nixtos/init/runit/default.nix
+++ nixtos/init/runit/default.nix
@@ -1,12 +1,25 @@
 { pkgs }:
 
-{ kernel ? "kernel" }:
+{
+  kernel ? "kernel",
+  files ? "files",
+}:
 
 extenders: [
   { extends = kernel;
     data = {
       type = "init";
       command = "${pkgs.runit}/bin/runit";
+    };
+  }
+
+  { extends = files;
+    data = {
+      type = "symlink";
+      file = "/etc/runit/1";
+      target = pkgs.writeScript "runit-1" ''
+        Hello world from runit initialization script
+      '';
     };
   }
 
