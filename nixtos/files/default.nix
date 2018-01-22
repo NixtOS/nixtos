f2929988ecf78c92e85ac4e8add5c5fb18f162b7
Léo Gaspard <leo@gaspard.io>
2018-01-22 04:42:48 +0100

    [files] Check that no two extension blocks refer to the same file

--- nixtos/files/default.nix
+++ nixtos/files/default.nix
@@ -6,16 +6,20 @@ extenders:
 
 [
   { extends = activation-scripts;
-    data = {
-      type = "script";
-      script = pkgs.lib.concatStringsSep "\n" (map (e:
-        ''mkdir -p "${baseNameOf e.file}"'' + "\n" + (
-          if e.type == "symlink" then
-            ''ln -s "${e.target}" "${e.file}"''
-          else
-            throw "Unknown type for generating file: ‘${e.type}’"
-        )
-      ) extenders);
-    };
+    data =
+      assert builtins.all (e:
+        1 == pkgs.lib.count (x: x.file == e.file) extenders
+      ) extenders;
+      {
+        type = "script";
+        script = pkgs.lib.concatStringsSep "\n" (map (e:
+          ''mkdir -p "${baseNameOf e.file}"'' + "\n" + (
+            if e.type == "symlink" then
+              ''ln -s "${e.target}" "${e.file}"''
+            else
+              throw "Unknown type for generating file: ‘${e.type}’"
+          )
+        ) extenders);
+      };
   }
 ]
