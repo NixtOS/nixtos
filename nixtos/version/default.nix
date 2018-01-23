e6d232dad74b4414d1dcba74a1c32180cb1568cb
Léo Gaspard <leo@gaspard.io>
2018-01-23 21:03:42 +0100

    [version] Allow overriding the commit id with a custom version file, for fewer rebuilds during development

--- nixtos/version/default.nix
+++ nixtos/version/default.nix
@@ -8,6 +8,9 @@ rec {
       ) 1
     );
   nixtos = with pkgs.lib;
-    substring 0 8 (commitIdFromGitRepo ../../.git);
+    if pathExists ./version then
+      fileContents ./version
+    else
+      substring 0 8 (commitIdFromGitRepo ../../.git);
   name = "nixtos-${nixtos}-nixpkgs-${nixpkgs}";
 }
