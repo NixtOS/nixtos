b8fd99c189b6393f26422509bab0ba9f0f585b6d
Léo Gaspard <leo@gaspard.io>
2018-01-25 19:59:09 +0100

    [solve-filesystems] Add a helper for getting only the dependencies of a path

--- nixtos/solve-filesystems/default.nix
+++ nixtos/solve-filesystems/default.nix
@@ -7,6 +7,9 @@ let
   # TODO(high): Actually handle mounting everything
   initrd-block-devices = filesystems."/".wait-for-block-devices;
 
+  # List of the mount points
+  mountpoints = builtins.attrNames filesystems;
+
   # Returns all parent directories (excluding the path itself) of a path.
   all-parents = path:
     if path == "/" then [ ]
@@ -22,18 +25,38 @@ let
     map all-parents (all-leaf-deps mount)
   );
 
-  # Sorts mount points by dependence order
-  sort-mount-points = mountpoints: (pkgs.lib.toposort (before: after:
-    builtins.elem before (all-path-deps after)
-  ) mountpoints).result;
+  # Returns true iff `b` must be mounted before `a` (ie. `a` depends on `b`)
+  depends-on = a: b: builtins.elem b (all-path-deps a);
 
-  # Returns a script that mounts all filesystems under `root`
-  mount-all = root:
+  # Returns all the parent mount points of a path (including itself).
+  all-parent-mount-points = path:
+    builtins.filter (x: builtins.elem x mountpoints)
+                    (all-parents path ++ [path]);
+
+  # Returns a script that mounts the passed filesystems in the order given
+  mount-fs-list = mountpoints: root:
     pkgs.lib.concatStringsSep "\n" (map (fs:
       "mkdir -p ${root}${fs}\n" +
       filesystems.${fs}.mount-command (root + fs)
-    ) (sort-mount-points (builtins.attrNames filesystems)));
+    ) mountpoints);
+
+  # The (ordered) list of all filesystems
+  all-filesystems = top.lib.sorted-deps-of depends-on mountpoints mountpoints;
+
+  # Returns a script that mounts all filesystems under `root`
+  mount-all = root: mount-fs-list all-filesystems root;
+
+  # Returns the (ordered) list of filesystems that have to be mounted in order
+  # to access `path`
+  filesystems-for = path:
+    top.lib.sorted-deps-of depends-on mountpoints (
+      all-parent-mount-points path
+    );
+
+  # Returns a script that mounts all the filesystems required in order to access
+  # `path`
+  mount-filesystems-for = path: root: mount-fs-list (filesystems-for path) root;
 in
 {
-  inherit initrd-block-devices mount-all;
+  inherit initrd-block-devices mount-all mount-filesystems-for;
 }
