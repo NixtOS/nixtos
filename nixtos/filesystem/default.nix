2ecdf0233ea38c5c9243e82003f8429ea3db223b
Léo Gaspard <leo@gaspard.io>
2018-01-26 01:56:41 +0100

    [virtfs] Add virtfs filesystem type

--- nixtos/filesystem/default.nix
+++ nixtos/filesystem/default.nix
@@ -2,4 +2,5 @@
 
 {
   ext4 = import ./ext4 { inherit pkgs top; };
+  virtfs = import ./virtfs { inherit pkgs top; };
 }
---
+++ nixtos/filesystem/virtfs/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, top }:
+
+{ tag }:
+
+# TODO(low): Figure out how to wait on something (file?) to be sure the fs
+# can be mounted before trying to mount it (but I can't imagine a way for the fs
+# not to be ready just after modprobe, so that's likely not a top-priority
+# issue)
+{
+  wait-for-block-devices = [];
+  wait-for-files = [];
+
+  extra-modules = [ "9p" "9pnet_virtio" "virtio_pci" ];
+
+  # Accoding to The Internet™, 256KiB (= 262144B) works pretty well
+  mount-command = mount:
+    "mount -t 9p -o trans=virtio,version=9p2000.L,msize=262144 ${tag} ${mount}";
+}
