48334f6c2d593a91039b03f8f680b9d89b30b84a
Nadrieril <nadrieril@gmail.com>
2018-02-07 17:47:05 +0000

    nixtos/version: get version from .git if nixpkgs does not point to a channel

--- nixtos/version/default.nix
+++ nixtos/version/default.nix
@@ -2,15 +2,21 @@
 
 rec {
   nixpkgs = with pkgs.lib;
-    substring 0 8 (
-      elemAt (
-        splitString "." (fileContents "${toString pkgs.path}/.version-suffix")
-      ) 1
-    );
+    if pathExists "${toString pkgs.path}/.version-suffix" then
+      substring 0 8 (
+        elemAt (
+          splitString "." (fileContents "${toString pkgs.path}/.version-suffix")
+        ) 1
+        )
+    else if pathExists "${toString pkgs.path}/.git" then
+      substring 0 8 (commitIdFromGitRepo "${toString pkgs.path}/.git")
+    else "unknown-version";
+
   nixtos = with pkgs.lib;
     if pathExists ./version then
       fileContents ./version
     else
       substring 0 8 (commitIdFromGitRepo ../../.git);
+
   name = "nixtos-${nixtos}-nixpkgs-${nixpkgs}";
 }
